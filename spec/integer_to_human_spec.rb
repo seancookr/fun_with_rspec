8fea85b4b59023b8a42c83d3f761c10b69064ae2
Sean Cook <seancookr@gmail.com>
2019-02-21 09:01:48 -0800

    remove some tests

--- spec/integer_to_human_spec.rb
+++ spec/integer_to_human_spec.rb
@@ -6,24 +6,7 @@ describe IntegerToHuman do
 
   context 'under 1000' do
     context 'aughts' do
-      it { expect(subject.to_human(0)).to eq 'Zero' }
       it { expect(subject.to_human(1)).to eq 'One' }
-    end
-
-    context 'teens' do
-      it { expect(subject.to_human(10)).to eq 'Ten' }
-    end
-
-    context 'multiples of 10' do
-
-    end
-
-    context 'multiples of 100' do
-
-    end
-
-    context 'composites' do
-
     end
   end
 
