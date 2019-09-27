5f2f4bf14c25e3a1d4cb5a2b955b3737f6d513d0
Sean Cook <seancookr@gmail.com>
2019-09-27 14:24:19 -0700

    all under 100 work

--- lib/integer_to_human.rb
+++ lib/integer_to_human.rb
@@ -3,9 +3,16 @@ class IntegerToHuman
              eleven twelve thirteen fourteen fifteen sixteen seventeen
              eighteen nineteen
             )
+
+  TENS = %w(wrong wrong twenty thirty forty fifty sixty seventy eighty ninety)
   class << self
     def to_human integer
-      SMALL[integer]
+      if integer < 20
+        SMALL[integer]
+      else
+        div, mod = integer.divmod(10)
+        TENS[div] + (mod.zero? ? '' : "-#{to_human(mod)}")
+      end
     end
   end
 end
--- spec/integer_to_human_spec.rb
+++ spec/integer_to_human_spec.rb
@@ -6,11 +6,25 @@ describe IntegerToHuman do
 
   context 'under 1000' do
     context 'aughts' do
-      it { expect(subject.to_human(0)).to eq 'zero' }
-      it { expect(subject.to_human(1)).to eq 'one' }
-      it { expect(subject.to_human(2)).to eq 'two' }
-      it { expect(subject.to_human(10)).to eq 'ten' }
-      it { expect(subject.to_human(19)).to eq 'nineteen' }
+      context 'unique' do
+        it { expect(subject.to_human(0)).to eq 'zero' }
+        it { expect(subject.to_human(1)).to eq 'one' }
+        it { expect(subject.to_human(2)).to eq 'two' }
+        it { expect(subject.to_human(10)).to eq 'ten' }
+        it { expect(subject.to_human(19)).to eq 'nineteen' }
+      end
+
+      context 'under 100' do
+        it { expect(subject.to_human(20)).to eq 'twenty' }
+        it { expect(subject.to_human(21)).to eq 'twenty-one' }
+        it { expect(subject.to_human(22)).to eq 'twenty-two' }
+        it { expect(subject.to_human(56)).to eq 'fifty-six' }
+        it { expect(subject.to_human(96)).to eq 'ninety-six' }
+      end
+
+      context 'under 1000' do
+
+      end
     end
   end
 
