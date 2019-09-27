8d9d319bcba790aa71d6d4d92ad42cef6a651b0c
Sean Cook <seancookr@gmail.com>
2019-09-27 14:12:39 -0700

    small numbers

--- lib/integer_to_human.rb
+++ lib/integer_to_human.rb
@@ -1,7 +1,11 @@
 class IntegerToHuman
+  SMALL = %w(zero one two three four five six seven eight nine ten
+             eleven twelve thirteen fourteen fifteen sixteen seventeen
+             eighteen nineteen
+            )
   class << self
     def to_human integer
-      'One'
+      SMALL[integer]
     end
   end
 end
--- spec/integer_to_human_spec.rb
+++ spec/integer_to_human_spec.rb
@@ -6,7 +6,11 @@ describe IntegerToHuman do
 
   context 'under 1000' do
     context 'aughts' do
-      it { expect(subject.to_human(1)).to eq 'One' }
+      it { expect(subject.to_human(0)).to eq 'zero' }
+      it { expect(subject.to_human(1)).to eq 'one' }
+      it { expect(subject.to_human(2)).to eq 'two' }
+      it { expect(subject.to_human(10)).to eq 'ten' }
+      it { expect(subject.to_human(19)).to eq 'nineteen' }
     end
   end
 
